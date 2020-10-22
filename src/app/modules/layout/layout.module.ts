479c47aef7325346fb97bfeca76ef7142617fee2
rohitsengar001 <rohitsengar001@gmail.com>
2020-10-23 00:43:35 +0530

    make home component and designed it to display as home page

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent,NavigationComponent],
+  declarations: [HeaderComponent,NavigationComponent, HomeComponent],
   imports: [
     CommonModule
   ],
